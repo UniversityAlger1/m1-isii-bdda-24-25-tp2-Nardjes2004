4fdd196cecccaad4610e7fbeb8e47c0d6627e736
Nardjes2004 <nardjesanane@gmail.com>
2025-04-23 18:26:41 +0100

    Test

--- 2_2_DateFin.sql
+++ 2_2_DateFin.sql
@@ -1,1 +1,9 @@
+-- Use LabDatabase
+USE LabDatabase;
+GO
+
+-- Remove DateFin column from Laboratoire table
+ALTER TABLE Laboratoire
+DROP COLUMN DatFin;
+GO
 
--- 3_3_allChercheurs.sql
+++ 3_3_allChercheurs.sql
@@ -1,1 +1,7 @@
+-- Use LabDatabase
+USE LabDatabase;
+GO
 
+-- Display all Chercheurs from laboratoire with CodeLab = '0001'
+SELECT NumCh, NomCh FROM Chercheur WHERE CodeLab = '0001';
+GO
